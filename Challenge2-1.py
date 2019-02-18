5d6ea0fab007a1f48499fb5ab0c45e0416d49d1d
Mercy Auma <aumagabriella@gmail.com>
2019-02-19 00:24:39 +0300

    Second challenge

---
+++ Challenge2-1.py
@@ -0,0 +1,11 @@
+print("Hello there")
+Name=input("What is your name?")
+YOB=input("Enter your year of birth: ")
+CD=2019
+X=CD-YOB
+if(X<18)
+print(Name+", you are a minor")
+if(X>18&&x<36)
+print(Name + ", you are a youth")
+if(>36)
+print(Name + ", you are an adult")
